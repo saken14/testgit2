6537f4c1b972cf8d88a96840af4fcb2d113bda92
RauanBimat <2016apolla@gmail.com>
2022-05-20 01:44:27 +0600

    second commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World Saken");
         System.out.println("Hello World Rauan");
+        System.out.println("Hello World Rauan2");
     }
 }
